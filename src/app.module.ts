d2eaaf572177880ff62ae008660bea9d6b4460f8
3darcart0 <efren.carrillo@rimac.com.pe>
2023-10-07 10:13:13 -0500

    feature(list-Vehicles): config vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehicleModule } from './vehicles/vehicle.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Vehicle } from './vehicles/vehicle.entity';
 import { Customer } from './entities/customer.entity';
 import { User } from './entities/user.entity';
@@ -17,13 +18,14 @@ import { Policy } from './entities/policy.entity';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'secret',
-      database: 'bsq_workshops',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: [Vehicle, Customer, User, Brand, Model, WorkshopRed, Workshop, Workshop, BranchOffice, BranchType, Group, Policy],
       synchronize: false,
       autoLoadEntities: true,
